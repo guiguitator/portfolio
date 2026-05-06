060a77df903ea6e6f090b30aa5c2f982d1c96345
gmeunier <rat.cornu16@gmail.com>
2026-05-06 09:16:20 +0200

    Minor bug fixes and changes to the text on the home page

--- src/Service/ProjectService.php
+++ src/Service/ProjectService.php
@@ -12,16 +12,29 @@ class ProjectService
         private ProjectRepository $projectRepository
     ) {}
 
+    /**
+     * Retrieves all projects, ordered by creation date in descending order.
+     * @return Project[] An array of all projects.
+     */
     public function getAllProjects(): array
     {
-        return $this->projectRepository->findAll();
+        return $this->projectRepository->findBy([], ['createdAt' => 'DESC']);
     }
 
+    /**
+     * Retrieves featured projects, ordered by creation date in descending order.
+     * @return Project[] An array of featured projects.
+     */
     public function getFeaturedProjects(): array
     {
-        return $this->projectRepository->findBy(['featured' => true]);
+        return $this->projectRepository->findBy(['featured' => true], ['createdAt' => 'DESC']);
     }
 
+    /**
+     * Retrieves a project by its ID.
+     * @param int $id The ID of the project to retrieve.
+     * @return Project The project with the specified ID.
+     */
     public function getProjectById(int $id): Project
     {
         return $this->projectRepository->findOneBy(['id' => $id]);
